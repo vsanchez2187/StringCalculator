01cac7b8f2f32f8d082214ce7ced99195ed687f2
Vladimir Sanchez Mondeja <vsanchez2187@gmail.com>
2020-01-15 12:00:42 +0100

    update travis destination

--- StringCalculatorTests/StringCalculatorTests.swift
+++ StringCalculatorTests/StringCalculatorTests.swift
@@ -12,16 +12,14 @@ import XCTest
 class StringCalculatorTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testPerformanceExample() {
@@ -30,5 +28,4 @@ class StringCalculatorTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
